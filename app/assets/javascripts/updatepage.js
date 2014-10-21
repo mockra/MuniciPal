9a7f45cf9fd68fd0e72039f096fedd605604079e
tom buckley <buckley.tom@gmail.com>
2014-10-21 13:14:11 -0700

    remove address from state push

--- app/assets/javascripts/updatepage.js
+++ app/assets/javascripts/updatepage.js
@@ -89,7 +89,7 @@ function update_with_new( data ) {
   if (data.in_district) {
 
     if (data.person_title == "councilmember") {
-      history.pushState({}, "", "?address=" + data.address + "&lat=" + data.lat + "&long=" + data.lng);
+      history.pushState({}, "", "&lat=" + data.lat + "&long=" + data.lng);
       marker.setLatLng(new L.LatLng(data.lat, data.lng));
       var district = _.find(districts, { id: data.district_id });
       var geoJSON = $.parseJSON(district.geom);
