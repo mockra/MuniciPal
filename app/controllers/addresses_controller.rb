0c41bd5c53158c697494422fb6397ff30c5f62e0
tom buckley <buckley.tom@gmail.com>
2014-10-23 10:42:06 -0700

    moving response hash to top of controller and setting vars along the way

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,50 +5,46 @@ class AddressesController < ApplicationController
   respond_to :html, :json
 
   def index
-    @in_district = false
-    @location = nil
-    @lat = nil
-    @lng = nil
-    @address = nil
-    @addr = ""
+#   fill this out and set it along the way
+    @response = { :lat                    => nil,
+                  :lng                    => nil,
+                  :address                => "",
+                  :in_district       => false,
+                  :person_title      => "",
+                  :district       => nil,
+                  :event_items       => nil,
+                  :attachments => nil,
+                  :events => nil
+                }
 
-    #fill this out and set it along the way
-    #if (['district', 'mayor', 'address', 'manager', 'lat', 'lon'] & params.keys).length > 0
-    # @response = { :lat                    => @lat,
-    #               :lng                    => @lng,
-    #               :address                => @addr,
-    #               :in_district       => @in_district,
-    #               :person_title      => @person_title,
-    #               :district_id       => @district_id,
-    #               :event_items       => @event_items,
-    #               :attachments => attachments,
-    #               :events => events
-    #             }
-
-# /people/byTitle/:title (title = mayor, manager, councilmember, all)
+    #move below to own route? - seem like key should be :title, not :title_value?
+    # /people/byTitle/:title (title = mayor, manager, councilmember, all)
     if params[:mayor]
-      @person_title = "mayor"
+      @response[:person_title] = "mayor"
     elsif params[:manager]
-      @person_title = "manager"
+      @response[:person_title] = "manager"
     else
-      @person_title = "councilmember"
+      @response[:person_title] = "councilmember"
     end
+
+    #SET THE VALUES IN THE RESPONSE TO THEIR VALUES IN THE PARAMS
+    @response.each_key{|key| @response[key.to_sym] = params[key.to_sym]}
 
     # district given
 #/districts/byId/:id/ -> { person + things}, where things = event_items (including attachments), and events
-    if not params[:district].blank?
+    if not @response[:district].blank?
 
-      @in_district = true
+      @response[:in_district] = true
 
-      if params[:district] == "all"
+      if @response[:district] == "all"
         # puts "mayor or manager!"
         # @mayor = true
         # @district_id = 0 # 0 means mayor
         #marker_location = [33.42, -111.835]
         # use lat/lon at center of Mesa
-        @lat = 33.42
-        @lng = -111.835
-        @location = { lat: @lat, lng: @lng }
+        @response[:lat] = 33.42
+        @response[:lng] = -111.835
+        @location = { lat: @response[:lat], lng: @response[:lng] }
       else
         # NEED TO REPLACE THIS
         # find lat/lon at center of polygon
@@ -65,35 +61,35 @@ class AddressesController < ApplicationController
 # /districts/byAddress/:address -> lat,lon
 
 
-    if not params[:address].blank?
+    if not @response[:address].blank?
     #if address is given:
-      @geocoded_address = Geokit::Geocoders::MultiGeocoder.geocode params[:address]
-      @lat = @geocoded_address.lat
+      @geocoded_address = Geokit::Geocoders::MultiGeocoder.geocode @response[:address]
+      @response[:lat] = @geocoded_address.lat
       @lng = @geocoded_address.lng
-      @location = { lat: @lat, lng: @lng }
-    elsif (not params[:lat].blank? and not params[:long].blank?)
+      @location = { lat: @response[:lat], lng: @response[:lng] }
+    elsif (not @response[:lat].blank? and not @response[:long].blank?)
     #if lat and long are given or geocoded from address
-      @lat = params[:lat]
-      @lng = params[:long]
-      @location = { lat: @lat, lng: @lng }
+      @response[:lat] = @response[:lat]
+      @response[:lng] = @response[:long]
+      @location = { lat: @response[:lat], lng: @response[:lng] }
     end
 
     if @location
-      @district = CouncilDistrict.getDistrict @location[:lat], @location[:lng] #@lat, @lng
-      @in_district = !@district.nil?
-      @district_id = @district.id if @in_district
+      @district_json = CouncilDistrict.getDistrict @location[:lat], @location[:lng] #@lat, @lng
+      @response[:in_district] = !@district_json[:district].nil?
+      @response[:district] = @district_json.id if @response[:in_district]
     end
 
-    if not params[:district].blank?
-      @district_id = params[:district]
-      @district = CouncilDistrict.find(@district_id)
-      @in_district = true
+    if not @response[:district].blank?
+      #NOT SURE WE NEED THIS LINE BELOW - THE JSON ISN'T USED FOR ANYTHING
+      @district_json = CouncilDistrict.find(@response[:district])
+      @response[:in_district] = true
     end
 
 
     if @district
       @event_items = EventItem.current.with_matters.in_district(@district.id).order('date DESC') +
-                     EventItem.current.with_matters.no_district.order('date DESC') unless @in_district == FALSE
+                     EventItem.current.with_matters.no_district.order('date DESC') if @response[:in_district]
     end
 
 
@@ -145,16 +141,16 @@ class AddressesController < ApplicationController
       events = @event_items.map(&:event).uniq #see http://ablogaboutcode.com/2012/01/04/the-ampersand-operator-in-ruby/
     end
 
-    @addr = @geocoded_address.full_address if @geocoded_address
+#    @addr = @geocoded_address.full_address if @geocoded_address
 
-    if @person_title == "mayor" or @person_title == "manager"
+    if @response[:person_title] == "mayor" or @response[:person_title] == "manager"
       @event_items = EventItem.current.with_matters.order('date DESC') #all
-      @district_id = nil
-        if !@lat or !@lng
-          @lat = 33.42
-          @lng = -111.835
+      @response[:district] = nil
+        if !@response[:lat] or !@response[:lng]
+          @response[:lat] = 33.42
+          @response[:lng] = -111.835
           #the following line is a legacy thing from a variable in JS that flags whether a user was in the city
-          @in_district = true;
+          @response[:in_district] = true;
           # @district_polygon = CouncilDistrict.getDistrict @lat, @lng
           # if @district_polygon and @district_polygon.id
           #   @district_id = @district_id.id
@@ -162,24 +158,23 @@ class AddressesController < ApplicationController
         end
     end
 
-
-
-    # only build a response if user asks for something specific
-    # the following line checks that the submitted parameters match at least one of the variables listed in the array
-    if (['district', 'mayor', 'address', 'manager', 'lat', 'lon'] & params.keys).length > 0
-      @response = { :lat                    => @location[:lat], #@lat,
-                    :lng                    => @location[:lng], #@lng,
-                    :address                => @addr,
-                    :in_district       => @in_district,
-                    :person_title      => @person_title,
-                    :district_id       => @district_id,
-                    :event_items       => @event_items,
-                    :attachments => attachments,
-                    :events => events
-                  }
-    else
-      @response = {}
-    end
+    #
+    # # only build a response if user asks for something specific
+    # # the following line checks that the submitted parameters match at least one of the variables listed in the array
+    # if (['district', 'mayor', 'address', 'manager', 'lat', 'lon'] & params.keys).length > 0
+    #   @response = { :lat                    => @location[:lat], #@lat,
+    #                 :lng                    => @location[:lng], #@lng,
+    #                 :address                => @addr,
+    #                 :in_district       => @in_district,
+    #                 :person_title      => @person_title,
+    #                 :district       => @district_id,
+    #                 :event_items       => @event_items,
+    #                 :attachments => attachments,
+    #                 :events => events
+    #               }
+    # else
+    #   @response = {}
+    # end
 
     respond_with(@response)
   end
